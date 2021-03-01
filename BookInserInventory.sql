3f69afba7c4d5af54b34f8e8b8acc3af3bceb950
mayank8903 <mayankraj2608@gmail.com>
2021-02-28 20:39:21 -0500

    made changes to database

--- BookInserInventory.sql
+++ BookInserInventory.sql
@@ -1,9 +1,9 @@
 USE BookStore;
 
-INSERT INTO `bookinventory` (`idBookInventory`, `book_name`, `book_quantity`, `book_image`, `book_price`) VALUES (NULL, 'The New Wilderness', 'book1', '10', '300');
-INSERT INTO `bookinventory` (`idBookInventory`, `book_name`, `book_quantity`, `book_image`, `book_price`) VALUES (NULL, 'This Mournable Body', 'book2', '8', '200');
-INSERT INTO `bookinventory` (`idBookInventory`, `book_name`, `book_quantity`, `book_image`, `book_price`) VALUES (NULL, 'Girl in White Cotton', 'book3', '9', '350');
-INSERT INTO `bookinventory` (`idBookInventory`, `book_name`, `book_quantity`, `book_image`, `book_price`) VALUES (NULL, 'Shuggie Bain', 'book4', '4', '2379');
-INSERT INTO `bookinventory` (`idBookInventory`, `book_name`, `book_quantity`, `book_image`, `book_price`) VALUES (NULL, 'A Promised Land', 'book5', '29', '105');
+INSERT INTO `bookinventory` (`idBookInventory`, `book_name`, `book_quantity`, `book_image`, `book_price`) VALUES (NULL, 'The New Wilderness', '10', 'book1','300');
+INSERT INTO `bookinventory` (`idBookInventory`, `book_name`, `book_quantity`, `book_image`, `book_price`) VALUES (NULL, 'This Mournable Body', '12','book2','200');
+INSERT INTO `bookinventory` (`idBookInventory`, `book_name`, `book_quantity`, `book_image`, `book_price`) VALUES (NULL, 'Girl in White Cotton', '8', 'book3','350');
+INSERT INTO `bookinventory` (`idBookInventory`, `book_name`, `book_quantity`, `book_image`, `book_price`) VALUES (NULL, 'Shuggie Bain', '8', 'book4', '279');
+INSERT INTO `bookinventory` (`idBookInventory`, `book_name`, `book_quantity`, `book_image`, `book_price`) VALUES (NULL, 'A Promised Land', '29', 'book5', '105');
 
 select * from bookinventory;
